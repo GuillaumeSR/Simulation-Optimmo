d461319b8168d371af370f9f1645dabfdbdb54ec
laguigzzz <guillaume.samson-richert@coda-student.school>
2024-03-18 13:58:40 +0100

    mcv architecture

---
+++ src/views/layout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            
+        </nav>
+    </header>
+    <main>
+        <?php require '../src/controllers/' . $route . '_controller.php'; ?>
+    </main>
+    <footer>
+        <p>&copy; 2024 Optimmo Simulator. All rights reserved.</p>
+    </footer>
+</body>
+</html>
