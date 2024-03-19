d6691dc30a2c7563b516395907ed8b7f2a5586e5
DavidChatton <davidchatton.dev@gmail.com>
2024-03-19 09:26:14 +0100

    adding homepage

---
+++ src/views/homepage.php
@@ -0,0 +1,34 @@
+<body>
+    <div class="background-homepage">
+        <p class="subtitle">Simulateur</p>
+        <h1><span> Obtenez votre plan de travaux</span> 
+            <span> personnalisé pour optimiser votre DPE</span></h1>
+        <div class="container-card">
+            <div class="card">
+                <div class="container-image">
+                <div class="green-circle"></div>
+                <img src="https://app.optimmo-energies.com/assets/dpe-example.3b4d1144.png" alt="DPE">
+            </div>
+            <div class="card-bottom">
+                <h3>J'ai déjà mon DPE</h3>
+                <p>Format PDF</p>
+            </div>
+        </div>
+
+            <div class="card">
+                <a href="?page=">  
+                <div class="container-image">
+                        <img src="https://app.optimmo-energies.com/assets/dpe-example.3b4d1144.png" alt="DPE">
+                        <div class="green-circle"></div>
+                        <div class="green-bar"></div>
+                </div>
+                <div class="card-bottom">
+                    <h3>Je n'ai pas encore de DPE</h3>
+                </div>
+            </div>
+        </div>
+        <button class="btn-continue"> 
+            <span>Continuer <i class="ri-arrow-right-line"></i></span>
+        </button>
+    </div> 
+</body>
