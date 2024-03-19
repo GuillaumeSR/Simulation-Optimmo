5f3940d8b5119914b9dc957f1b19ae79664f7ec1
DavidChatton <davidchatton.dev@gmail.com>
2024-03-19 14:16:24 +0100

    adding function on homepage and add button parameter on layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-homepage
-addEventListener('click',)
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll(".card");
+    const button = document.querySelector(".btn-continue");
 
+    cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            cards.forEach(function(otherCard) {
+                if (otherCard !== card) {
+                    otherCard.style.border = "none";
+                }
+            });
+            card.style.border = "1px solid #07755A";
+            button.style.backgroundColor = "#07755A";
+        });
+    });
+});
